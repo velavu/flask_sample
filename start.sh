851b50b6a8da42f003b5d7b1502a783440f906e3
velappana <velappana@nbnco.com.au>
2018-03-25 19:48:03 +1100

    [deploy] added master script -and ugraded to version1.1

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-kill $(ps aux | grep 'app.py' | awk '{print $2}')
 cd /opt/flask_sample/
+kill $(ps aux | grep 'app.py' | awk '{print $2}')
+chmod 755 *
 git pull
+chmod 755 *
 python app.py > flask.log &
